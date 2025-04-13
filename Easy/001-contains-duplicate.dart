1f5e95aefdfffc0df7a9783bcadb2517e5ffe129
Joy Anis00 <joyanis00l@gmail.com>
2025-04-13 22:15:20 +0200

    first commit

--- Easy/001-contains-duplicate.dart
+++ Easy/001-contains-duplicate.dart
@@ -2,17 +2,13 @@
 //Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
 void main() {
-  print(containsDuplicate([1, 2, 3, 4, 5, 6, 7, 8, 1]));
+  print(containsDuplicate([1, 2, 3, 4, 5, 6, 7, 8]));
 }
 
 bool containsDuplicate(List<int> nums) {
-  Set seen = {};
-  for (var element in nums) {
-    if (!seen.contains(element)) {
-      seen.add(element);
-    } else {
-      return true;
-    }
-  }
+
+if (nums.length > nums.toSet().length){
+  return true;
+}
   return false;
 }
